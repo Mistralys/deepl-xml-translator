21adf6ccbb4a9e255dbc786dbbb7f40a43497aa3
Mistralys <s.mordziol@gmail.com>
2022-04-26 08:37:31 +0200

    Code formatting.

--- tests/apikey.dist.php
+++ tests/apikey.dist.php
@@ -1,26 +1,26 @@
 <?php
 
-    // rename this file to apikey.php to use it.
+// rename this file to apikey.php to use it.
 
-    // set your API key here to test the translation live.
-    define('TESTS_DEEPL_APIKEY', '');
+// set your API key here to test the translation live.
+const TESTS_DEEPL_APIKEY = '';
 
-    
-    // define a proxy URI to enable the proxy-related tests.
-    // URI in the format: protocol://username:password@1.2.3.4:10
-    $proxyURI = '';
-    
-    // proxy URI variant with username and password
-    /*
-    $proxyURI = sprintf(
-        '%s://%s:%s@%s:%s',
-        'http',                // protocol (http/tcp)
-        urlencode('username'), // username
-        urlencode('password'), // password
-        'proxy.server',        // host
-        1234                   // port
-    );
-    */
-    
-    define('TESTS_PROXY_SERVER', $proxyURI);
-    
+
+// define a proxy URI to enable the proxy-related tests.
+// URI in the format: protocol://username:password@1.2.3.4:10
+$proxyURI = '';
+
+// proxy URI variant with username and password
+/*
+$proxyURI = sprintf(
+    '%s://%s:%s@%s:%s',
+    'http',                // protocol (http/tcp)
+    urlencode('username'), // username
+    urlencode('password'), // password
+    'proxy.server',        // host
+    1234                   // port
+);
+*/
+
+define('TESTS_PROXY_SERVER', $proxyURI);
+
